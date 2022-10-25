08f6b0939d857e18e18e5002861d84e22a00334a
pg-7v <dmkseev@gmail.com>
2022-10-25 14:08:50 +0300

    fix: settings.py

--- pomidor_shop/settings.py
+++ pomidor_shop/settings.py
@@ -10,12 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
+# import os
 from pathlib import Path
-
-from dotenv import load_dotenv
-
-load_dotenv()
+#
+# from dotenv import load_dotenv
+#
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = load_dotenv(os.getenv("SECRET_KEY"))
+SECRET_KEY='django-i111nsecure-%_b#son1s#e20rd4tg%)sn%xnh@*$9o&#4)85#k!#6w3#vio#&'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
