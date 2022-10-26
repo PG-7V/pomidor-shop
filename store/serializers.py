9fda92a571c6c9175efbd9a131b4e36d4b6a28e5
pg-7v <dmkseev@gmail.com>
2022-10-26 17:03:35 +0300

    feat: add test like and count like

--- store/serializers.py
+++ store/serializers.py
@@ -1,14 +1,19 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from store.models import Book, UserBookRelation
 
 
 class BooksSerializer(ModelSerializer):
+    likes_count = SerializerMethodField()
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ('id', 'name', 'price', 'author_name', 'likes_count')
+
+    def get_likes_count(self, instance):
+        return UserBookRelation.objects.filter(book=instance, like=True).count()
+
 
 class UserBookRelationSerializer(ModelSerializer):
     class Meta:
         model = UserBookRelation
-        fields = ('book', 'like', 'in_bookmarks', 'rate')
+        fields = '__all__'
