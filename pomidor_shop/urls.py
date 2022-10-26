1d0b251147f674c9ecc0f9dd8ed200b71b1c31c9
pg-7v <dmkseev@gmail.com>
2022-10-26 15:27:24 +0300

    feat: add like to model

--- pomidor_shop/urls.py
+++ pomidor_shop/urls.py
@@ -18,11 +18,12 @@ from django.urls import path, include
 # from django.conf.urls import url
 from rest_framework.routers import SimpleRouter
 
-from store.views import BookViewSet, auth
+from store.views import BookViewSet, auth, UserBooksRelationView
 
 router = SimpleRouter()
 
 router.register(r'book', BookViewSet)
+router.register(r'book_relation', UserBooksRelationView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
